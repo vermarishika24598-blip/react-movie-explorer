00ed0fc30e339bfc0b7275a8f85e8907bb7c976e
Rishika Verma <vermarishika24598@gmail.com>
2026-01-23 18:17:28 +0530

    Update API base URL to use import.meta.env

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Use environment variable for backend URL
 const API = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL 
+  baseURL: import.meta.env.REACT_APP_BACKEND_URL 
 });
 
 API.interceptors.request.use((req) => {
