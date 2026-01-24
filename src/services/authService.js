fd98e8cde006a27546136c3c94d5e7ba96ac1cc0
Rishika Verma <vermarishika24598@gmail.com>
2026-01-24 11:42:43 +0530

    Update API paths to /api/auth for backend compatibility

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,19 +1,19 @@
 import API from "../api/api";
 
 export const signup = async (formData) => {
-  const res = await API.post("/auth/signup", formData);
+  const res = await API.post("/api/auth/signup", formData);
   localStorage.setItem("token", res.data.token);
   return res.data;
 };
 
 export const signin = async (formData) => {
-  const res = await API.post("/auth/signin", formData);
+  const res = await API.post("/api/auth/signin", formData);
   localStorage.setItem("token", res.data.token);
   return res.data;
 };
 
 export const getMe = async () => {
-  const res = await API.get("/auth/me");
+  const res = await API.get("/api/auth/me");
   return res.data;
 };
 
