3c989ee92ef7fd83e7565e04db4ec4402fd4f8e6
Rishika Verma <vermarishika24598@gmail.com>
2026-01-24 17:24:58 +0530

    Update Popular.jsx

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -112,7 +112,7 @@ export default function Popular() {
 
       {/* Movies Grid */}
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        {coming.map((movie) => (
+        {popular.map((movie) => (
           <div
             key={movie.id}
             onClick={() => navigate(`/movie/${movie.id}`)}
